f53d4b05d63c3d0dc0810437c2fa92c7382089b4
adogbeji <baka_1993@yahoo.co.uk>
2023-06-28 20:44:14 +0100

    Add more styling to text on buyers home screen

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -6,10 +6,17 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top, left: 25, right: 15),
+      padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top, left: 25, right: 15),
       child: Row(
         children: <Widget>[
-          Text('Hi, what are you looking for?'),
+          Text(
+            'Hi, what are you looking for?',
+            style: TextStyle(
+              fontSize: 19,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ],
       ),
     );
