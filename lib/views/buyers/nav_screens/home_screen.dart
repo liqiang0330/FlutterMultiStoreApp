5bae8bb2bc7d9105bae18c3217e879a07a40d884
adogbeji <baka_1993@yahoo.co.uk>
2023-07-13 20:44:35 +0100

    Put custom widget in separate file and import to buyers home screen

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import './widgets/welcome_text_widget.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -33,34 +35,6 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class WelcomeText extends StatelessWidget {
-  const WelcomeText({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Text(
-          'Hi, what are you looking for? 👀',
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        Container(
-          child: SvgPicture.asset(
-            'assets/icons/cart.svg',
-            width: 20,
-          ),
-        ),
-      ],
     );
   }
 }
