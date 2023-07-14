8591328c267090034a56f1ac4331a6085127505d
adogbeji <baka_1993@yahoo.co.uk>
2023-07-14 20:42:32 +0100

    Change name of custom class for search bar

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -17,15 +17,15 @@ class HomeScreen extends StatelessWidget {
           SizedBox(
             height: 14,
           ),
-          SearchInput(),
+          SearchInputWidget(),
         ],
       ),
     );
   }
 }
 
-class SearchInput extends StatelessWidget {
-  const SearchInput({
+class SearchInputWidget extends StatelessWidget {
+  const SearchInputWidget({
     Key? key,
   }) : super(key: key);
 
