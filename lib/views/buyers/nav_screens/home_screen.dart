b4f6d91c20ab32e513214801725172574f4eb849
adogbeji <baka_1993@yahoo.co.uk>
2023-07-14 20:36:30 +0100

    Extract searcg bar inot custom widget in buyers home screen

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -14,26 +14,39 @@ class HomeScreen extends StatelessWidget {
       child: Column(
         children: [
           WelcomeText(),
-          SizedBox(height: 14,),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: TextField(
-              decoration: InputDecoration(
-                // fillColor: Colors.white,
-                fillColor: Colors.grey.shade300,
-                filled: true,
-                hintText: 'Search For Products',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                ),
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(14.0),
-                  child: SvgPicture.asset('assets/icons/search.svg', width: 10,),
-                ),
-              ),
-            ),
+          SizedBox(
+            height: 14,
           ),
+          SearchInput(),
         ],
+      ),
+    );
+  }
+}
+
+class SearchInput extends StatelessWidget {
+  const SearchInput({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: TextField(
+        decoration: InputDecoration(
+          // fillColor: Colors.white,
+          fillColor: Colors.grey.shade300,
+          filled: true,
+          hintText: 'Search For Products',
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: SvgPicture.asset('assets/icons/search.svg', width: 10,),
+          ),
+        ),
       ),
     );
   }
