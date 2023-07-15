41e3d393d138f9d416a7ae1ff4629f63289adc13
adogbeji <baka_1993@yahoo.co.uk>
2023-07-15 20:51:17 +0100

    Put search bar widget into separate file & import to buyers home screen

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import './widgets/welcome_text_widget.dart';
+import './widgets/search_input_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -24,30 +25,30 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class SearchInputWidget extends StatelessWidget {
-  const SearchInputWidget({
-    Key? key,
-  }) : super(key: key);
+// class SearchInputWidget extends StatelessWidget {
+//   const SearchInputWidget({
+//     Key? key,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: TextField(
-        decoration: InputDecoration(
-          // fillColor: Colors.white,
-          fillColor: Colors.grey.shade300,
-          filled: true,
-          hintText: 'Search For Products',
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none,
-          ),
-          prefixIcon: Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: SvgPicture.asset('assets/icons/search.svg', width: 10,),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return ClipRRect(
+//       borderRadius: BorderRadius.circular(10),
+//       child: TextField(
+//         decoration: InputDecoration(
+//           // fillColor: Colors.white,
+//           fillColor: Colors.grey.shade300,
+//           filled: true,
+//           hintText: 'Search For Products',
+//           border: OutlineInputBorder(
+//             borderSide: BorderSide.none,
+//           ),
+//           prefixIcon: Padding(
+//             padding: const EdgeInsets.all(14.0),
+//             child: SvgPicture.asset('assets/icons/search.svg', width: 10,),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
