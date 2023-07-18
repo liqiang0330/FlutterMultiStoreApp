dde3585e698bf07b10d7745ae6f98cca62b4b1de
adogbeji <baka_1993@yahoo.co.uk>
2023-07-18 20:44:02 +0100

    Change Application ID

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -24,31 +24,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
-// class SearchInputWidget extends StatelessWidget {
-//   const SearchInputWidget({
-//     Key? key,
-//   }) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ClipRRect(
-//       borderRadius: BorderRadius.circular(10),
-//       child: TextField(
-//         decoration: InputDecoration(
-//           // fillColor: Colors.white,
-//           fillColor: Colors.grey.shade300,
-//           filled: true,
-//           hintText: 'Search For Products',
-//           border: OutlineInputBorder(
-//             borderSide: BorderSide.none,
-//           ),
-//           prefixIcon: Padding(
-//             padding: const EdgeInsets.all(14.0),
-//             child: SvgPicture.asset('assets/icons/search.svg', width: 10,),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
