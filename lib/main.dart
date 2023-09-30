951a893a8ff68d2648926aa127844c876e445585
adogbeji <baka_1993@yahoo.co.uk>
2023-09-30 20:50:49 +0100

    Initialise Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import './views/buyers/main_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  // Initialises Flutter widgets BEFORE initialising Firebase (below)
+  await Firebase.initializeApp();  // Initialises Firebase
   runApp(const MyApp());
 }
 
